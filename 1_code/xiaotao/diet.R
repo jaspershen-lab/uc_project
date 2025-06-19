e35ca85bbb970569f0ad7ed892ad508265d360af
jaspershen <shenxt1990@outlook.com>
2025-06-19 17:09:42 +0800

    update

--- 1_code/xiaotao/diet.R
+++ 1_code/xiaotao/diet.R
@@ -27,13 +27,25 @@ data_validation1[c(2,4),6:8] %>%
   chisq.test()
 
 
-data_validation1 <-
-  readxl::read_xlsx("2_data/diet K方检验.xlsx", sheet = 3, col_names = FALSE)
+data_validation2 <-
+  readxl::read_xlsx("2_data/diet K方检验.xlsx", sheet = 4, col_names = FALSE)
 
-data_validation1[c(2,4),2:4] %>%
+data_validation2[c(2,4),2:4] %>%
   as.data.frame() %>%
   chisq.test()
 
-data_validation1[c(2,4),6:8] %>%
+data_validation2[c(2,4),6:8] %>%
+  as.data.frame() %>%
+  chisq.test()
+
+
+data_validation3 <-
+  readxl::read_xlsx("2_data/diet K方检验.xlsx", sheet = 5, col_names = FALSE)
+
+data_validation3[c(2,4),2:4] %>%
+  as.data.frame() %>%
+  chisq.test()
+
+data_validation3[c(2,4),6:8] %>%
   as.data.frame() %>%
   chisq.test()
